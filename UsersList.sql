379b9a28686b7efd51699c24beabe85673cf1a44
ElusiveKoala <htsai2@lsu.edu>
2018-04-20 15:21:46 -0500

    Add files via upload

---
+++ UsersList.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    UserID int,
+    Username varchar(255),
+    UserEmail varchar(255),
+    UserPass varchar(255),
+);
+
+INSERT INTO users (UserID, Username, UserEmail, UserPass)
+VALUES (0,'Caius','desertprince@hotmail.com','mfboom3'), (1, 'admin', 'admin@gameframe.com', 'admin');
