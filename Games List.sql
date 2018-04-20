b501c07e236b6c5b175da62c1ea2f740578e10c9
ElusiveKoala <htsai2@lsu.edu>
2018-04-20 15:21:28 -0500

    Update Games List.sql

--- Games List.sql
+++ Games List.sql
@@ -1,13 +1,3 @@
-CREATE TABLE users (
-    UserID int,
-    Username varchar(255),
-    UserEmail varchar(255),
-    UserPass varchar(255),
-);
-
-INSERT INTO users (UserID, Username, UserEmail, UserPass)
-VALUES (0,'Caius','desertprince@hotmail.com','mfboom3');
-
 CREATE TABLE games (
     GameID int,
     GameName varchar(255)
