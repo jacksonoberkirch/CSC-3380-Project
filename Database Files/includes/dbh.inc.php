708cda889a6bdb1080d135d16ed2c87094386cd1
agrem28 <35739364+agrem28@users.noreply.github.com>
2018-04-23 20:08:50 -0500

    Add the database files

---
+++ Database Files/includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "GameFrameDB";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
